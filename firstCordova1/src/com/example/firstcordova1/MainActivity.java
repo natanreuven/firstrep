16cc76fd23bfc364a54dab9cb2aca55bfc3d3f67
natanreuven <natanreuven@gmail.com>
2012-11-11 13:41:25 +0200

    comment for check

--- firstCordova1/src/com/example/firstcordova1/MainActivity.java
+++ firstCordova1/src/com/example/firstcordova1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        //first change
     }
 
     @Override
